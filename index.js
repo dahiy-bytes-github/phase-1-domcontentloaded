34e5a69cf97e4c5b3ae4a4801b2ad18d57fa763a
Abdullahi <aadahiye@gmail.com>
2024-10-08 22:04:42 +0300

    completes lab on DOMContentLOad

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',()=>{
+    updateDom()
+    
+})
+function updateDom(){
+    p = document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
